6f15d6511e900de8a8b3457f3ec1f9059141db09
Brunowcst <bruno.costa.079@ufrn.edu.br>
2023-06-20 21:22:04 -0300

    Gera inserts aletórios

--- orm/generator.py
+++ orm/generator.py
@@ -1,54 +1,59 @@
 from faker import Faker
 import random
+from models import *
 
 fake = Faker()
 
-def createDatas(conn, qntInserts):
+def createDatas(qntInserts, cur):
     try:
         for _ in range(qntInserts):
-            createFuncionario()
-            createDepto()
-            createProjeto()
-            createAtividade()
-            createMembro()
-            createAtividadeMembro()
-            createEquipe()
+            createFuncionario(cur)
+            print("func passou")
+            createDepto(cur)
+            print("depto")
+            createProjeto(cur)
+            print("proj")
+            createAtividade(cur)
+            print("ativ")
+            # createMembro()
+            createAtividadeMembro(cur)
+            print("ativMem")
+            createEquipe(cur)
+            print("equipes")
             
-        conn.commit()
+        cur.execute('COMMIT')
         print('Dados inseridos com sucesso.')
 
     except Exception as error:
-        print('Erro ao inserir dados na tabela funcionario:', error)
+        print('Erro ao inserir dados:', error)
 
-def createFuncionario():
-    codigo = random.randint(1000, 9999)
+def createFuncionario(cur):
     nome = fake.name()
     sexo = random.choice(['M', 'F'])
-    dataNasc = fake.date_of_birth(minimum_age=18, maximum_age=65)
+    datanasc = fake.date_of_birth(minimum_age=18, maximum_age=65)
     salario = random.uniform(1000, 5000)
     supervisor = random.choice(Funcionario.select())
     depto = random.choice(Departamento.select())
-    Funcionario.create(codigo=codigo, nome=nome, sexo=sexo, dataNasc=dataNasc, salario=salario, supervisor=supervisor, depto=depto)
+    Funcionario.create(nome=nome, sexo=sexo, datanasc=datanasc, salario=salario, supervisor=supervisor, depto=depto)
 
-def createDepto():
-    codigo = random.randint(1000, 9999)
+def createDepto(cur):
     sigla = fake.random_element(['FIN', 'HR', 'IT', 'SALES'])
     descricao = fake.sentence()
-    gerente = random.choice(Funcionario.select())
-    Departamento.create(codigo=codigo, sigla=sigla, descricao=descricao, gerente=gerente)
+    gerente = random.choice(Funcionario)
+    Departamento.create(sigla=sigla, descricao=descricao, gerente=gerente)
 
-def createEquipe():
+def createEquipe(cur):
     codigo = random.randint(1000, 9999)
-    nomeEquipe = fake.random_element(['Equipe A', 'Equipe B', 'Equipe C'])
-    Equipe.create(codigo=codigo, nomeEquipe=nomeEquipe)
+    nomeequipe = fake.random_element(['Equipe A', 'Equipe B', 'Equipe C'])
+    Equipe.create(codigo=codigo, nomeequipe=nomeequipe)
 
-def createMembro():
-    codigo = random.randint(1000, 9999)
-    codEquipe = random.choice(Equipe.select())
-    codFuncionario = random.choice(Funcionario.select())
-    Membro.create(codigo=codigo, codEquipe=codEquipe, codFuncionario=codFuncionario)
+# def createMembro(cur):
+#     codigo = random.randint(1000, 9999)
+#     codEquipe = random.choice(Equipe.select())
+#     codFuncionario = random.choice(Funcionario.select())
+#     Membro.create(codigo=codigo, codEquipe=codEquipe, codFuncionario=codFuncionario)
 
-def createProjeto():
+def createProjeto(cur):
     codigo = random.randint(1000, 9999)
     descricao = fake.sentence()
     depto = random.choice(Departamento.select())
@@ -62,7 +67,7 @@ def createProjeto():
     equipe = random.choice(Equipe.select())
     Projeto.create(codigo=codigo, descricao=descricao, depto=depto, responsavel=responsavel, dataInicio=dataInicio, dataFim=dataFim, situacao=situacao, dataConclusao=dataConclusao, equipe=equipe)
 
-def createAtividade():
+def createAtividade(cur):
     codigo = random.randint(1000, 9999)
     descricao = fake.sentence()
     dataInicio = fake.date_between(start_date='-1y', end_date='today')
@@ -73,12 +78,12 @@ def createAtividade():
         dataConclusao = fake.date_between(start_date=dataFim, end_date='today')
     Atividade.create(codigo=codigo, descricao=descricao, dataInicio=dataInicio, dataFim=dataFim, situacao=situacao, dataConclusao=dataConclusao)
 
-def createAtividadeMembro():
-    codAtividade = random.choice(atividades)
-    codProjeto = random.choice(projetos)
+def createAtividadeMembro(cur):
+    codAtividade = random.choice(Atividade)
+    codProjeto = random.choice(Projeto)
     AtividadeMembro.create(codAtividade=codAtividade, codProjeto=codProjeto)
 
-def createAtividadeProjeto():
-    codAtividade = random.choice(atividades)
-    codProjeto = random.choice(projetos)
+def createAtividadeProjeto(cur):
+    codAtividade = random.choice(Atividade)
+    codProjeto = random.choice(Projeto)
     AtividadeProjeto.create(codAtividade=codAtividade, codProjeto=codProjeto)
